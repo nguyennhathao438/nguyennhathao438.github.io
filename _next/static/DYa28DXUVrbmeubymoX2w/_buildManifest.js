038513a1c370c52d9ff9086f9aa8f94e69d386b3
Hao Nguyen <hao362k5@gmail.com>
2026-03-10 19:30:37 +0700

    Updates

--- _next/static/DYa28DXUVrbmeubymoX2w/_buildManifest.js
+++ _next/static/DYa28DXUVrbmeubymoX2w/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/pofolio//_next/:path+",
-        "destination": "/pofolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
